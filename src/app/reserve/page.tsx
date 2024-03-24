3161a936a75270215b8fc4b94c2b29faaf6beac6
hoshioka <chiruchiruno99@gmail.com>
2024-03-24 20:24:08 +0700

    fixed color

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -48,7 +48,7 @@ export default function Reserve (){
     return(
         <main className="w-[100%] flex flex-col items-center space-y-1 h-[850px]">
             <title>Reserve a restaurant</title>
-            <h1 className='text-4xl font-bold text-orange-200 m-20 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] '>Restaurant Reservation</h1>
+            <h1 className='text-4xl font-semi-bold text-emerald-600 m-20 bg-white-100 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] '>Restaurant Reservation</h1>
 
             {/* <div className="bg-slate-500 m-5 p-5 rounded-lg">
             <div className="text-2xl text-orange-200 text-center ">{profile.data.name}'s Information</div>
@@ -61,13 +61,13 @@ export default function Reserve (){
             </div> */}
 
             <div className="py-4">
-                <Box component="form" sx={{'& > :not(style)': { m: 1, width: '500px' },}} className="bg-slate-100 rounded-lg" >
+                <Box component="form" sx={{'& > :not(style)': { m: 1, width: '500px' },}} className="bg-rose-100 rounded-lg" >
                     <TextField id="name-lastname" name="Name-Lastname" label="Name-Lastname" variant="standard" value={name} onChange={(e)=> {setName(e.target.value)}}/>
                 </Box>
             </div>
 
             <div className="py-4">
-                <Box component="form" sx={{'& > :not(style)': { m: 1, width: '500px' },}} className="bg-slate-100 rounded-lg" >
+                <Box component="form" sx={{'& > :not(style)': { m: 1, width: '500px' },}} className="bg-rose-100 rounded-lg" >
                     <TextField id="name-lastname" name="CitizenID" label="Citizen ID" variant="standard" value={id} onChange={(e)=> {setId(e.target.value)}}/>
                 </Box>
             </div>
@@ -76,7 +76,7 @@ export default function Reserve (){
 
             <div className="w-fit space-y-2 py-6">
                 <div className="text-md text-gray-600 text-center">Select Restaurant</div>
-                <Select variant="standard" name="location" id="location" value={location} onChange={(e)=> {setLocation(e.target.value)}} className="h-[2em] w-[200px] bg-slate-100 rounded-lg py-2 px-2">
+                <Select variant="standard" name="location" id="location" value={location} onChange={(e)=> {setLocation(e.target.value)}} className="h-[2em] w-[200px] bg-rose-100 rounded-lg py-2 px-2">
                     <MenuItem value="Chula">Chulalongkorn Hospital</MenuItem>
                     <MenuItem value ="Rajavithi"> Rajavithi Hospital</MenuItem>
                     <MenuItem value ="Thammasat">Thammasat University Hospital</MenuItem>
@@ -84,11 +84,11 @@ export default function Reserve (){
             </div>
 
             <div className="w-fit space-y-2 py-4">
-                <div className="text-md text-center text-gray-600">Pick a date</div>
+                <div className="text-md text-center text-gray-600 ">Pick a date</div>
                 <DateReserve onDateChange={(value:Dayjs)=>{setBookDate(value)}}/>
             </div>
 
-            <button name="Book Vaccine" className="block rounded-md bg-sky-600 hover:bg-sky-700 transition px-3 py-2 space-y-5 text-white shadow-sm" onClick={Booking}>Reserve!</button>
+            <button name="Book Vaccine" className="block rounded-md bg-rose-600 hover:bg-rose-700 transition px-3 py-2 space-y-5 text-white shadow-sm" onClick={Booking}>Reserve!</button>
             
         </main>
     )
--- src/components/DateReserve.tsx
+++ src/components/DateReserve.tsx
@@ -10,7 +10,7 @@ export default function DateReserve({onDateChange}:{onDateChange:Function}){
 
     const [bookDate, setBookDate] = useState<Dayjs | null> (null)
     return(
-        <div className="bg-slate-100 rounded-lg space-x-5 space-y-2 w-fit px-10 py-5 flex flex-row justify-center">
+        <div className="bg-rose-100 rounded-lg space-x-5 space-y-2 w-fit px-10 py-5 flex flex-row justify-center">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DatePicker className="bg-white"
                 value={bookDate}
